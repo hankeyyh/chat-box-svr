039c63bece35a002b49eb61a9a57cce7e56ee072
hanke <hankeyyh@outlook.com>
2025-09-21 15:30:43 +0800

    feat: gorm gen model

---
+++ server/script/create_model/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/hankeyyh/chat-box-svr/conf"
+	"gorm.io/driver/mysql"
+	"gorm.io/gen"
+	"gorm.io/gorm"
+)
+
+func main() {
+	g := gen.NewGenerator(gen.Config{
+		OutPath: "model",
+		ModelPkgPath: "model/table",
+		Mode: gen.WithDefaultQuery,
+	})
+	gormdb, err := gorm.Open(mysql.Open(conf.DefaultConf.MysqlConf.GetDsn()))
+	if err != nil {
+		panic(err)
+	}
+	g.UseDB(gormdb)
+	g.ApplyBasic(
+		g.GenerateModel("session_v2"),
+		g.GenerateModel("app"),
+	)
+	g.Execute()
+}
